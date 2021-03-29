9eb5a90bf683b32c4e82e9b1f4e588edaac45747
kshama <kshamamd@gmail.com>
2021-03-29 18:11:08 +0530

    first commit

---
+++ TicTacToe.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("Welcome to TicTacToe game!!");
+    }
+}
